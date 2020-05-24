16938075d7e97185597ce8943379614976fa480b
Gordon Inggs <gordon.e.inggs@ieee.org>
2020-05-24 11:41:26 +0200

    Packing up code
    * Mostly moving things around.
    * Split out the command line scripting from library functions (bin/geocode-array and geocode_array/geocode_array.py).
    * Adding module file (__init__.py)
    * Adding setup.py
    * Updated README

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    version="0.1.0",
+    author=["Matthew Adendorff", "Heiko Heilgendorff"],
+    author_email="matthew@opencitieslab.org>",
+    description="Geocode cross-referencing using several unique services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/opendatadurban/geocode-array",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    scripts=['bin/geocode-array']
+)
